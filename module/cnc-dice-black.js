c92a988777d1f2238d3cc5d1c972e3e18bf79622
Joseph <JPMeehan5@Gmail.com>
2022-07-29 23:29:44 -0700

    Die label fix
    Now calling 1da or 1db in chat will get the correct icons

--- module/cnc-dice-black.js
+++ module/cnc-dice-black.js
@@ -14,18 +14,18 @@ export class CoyoteDiceBlack extends Die {
     /** @override */
     getResultLabel(result) {
         return {
-            "1": '<img src="systems/coyote-and-crow/ui/dice/b1.png" />',
-            "2": '<img src="systems/coyote-and-crow/ui/dice/b2.png" />',
-            "3": '<img src="systems/coyote-and-crow/ui/dice/b3.png" />',
-            "4": '<img src="systems/coyote-and-crow/ui/dice/b4.png" />',
-            "5": '<img src="systems/coyote-and-crow/ui/dice/b5.png" />',
-            "6": '<img src="systems/coyote-and-crow/ui/dice/b6.png" />',
-            "7": '<img src="systems/coyote-and-crow/ui/dice/b7.png" />',
-            "8": '<img src="systems/coyote-and-crow/ui/dice/b8.png" />',
-            "9": '<img src="systems/coyote-and-crow/ui/dice/b9.png" />',
-            "10": '<img src="systems/coyote-and-crow/ui/dice/b10.png" />',
-            "11": '<img src="systems/coyote-and-crow/ui/dice/b11.png" />',
-            "12": '<img src="systems/coyote-and-crow/ui/dice/b12.png" />',
+            "1": '<img src="systems/coyote-and-crow/ui/dice/chat/b1.png" />',
+            "2": '<img src="systems/coyote-and-crow/ui/dice/chat/b2.png" />',
+            "3": '<img src="systems/coyote-and-crow/ui/dice/chat/b3.png" />',
+            "4": '<img src="systems/coyote-and-crow/ui/dice/chat/b4.png" />',
+            "5": '<img src="systems/coyote-and-crow/ui/dice/chat/b5.png" />',
+            "6": '<img src="systems/coyote-and-crow/ui/dice/chat/b6.png" />',
+            "7": '<img src="systems/coyote-and-crow/ui/dice/chat/b7.png" />',
+            "8": '<img src="systems/coyote-and-crow/ui/dice/chat/b8.png" />',
+            "9": '<img src="systems/coyote-and-crow/ui/dice/chat/b9.png" />',
+            "10": '<img src="systems/coyote-and-crow/ui/dice/chat/b10.png" />',
+            "11": '<img src="systems/coyote-and-crow/ui/dice/chat/b11.png" />',
+            "12": '<img src="systems/coyote-and-crow/ui/dice/chat/b12.png" />',
         }[result.result];
     }
 }
--- module/cnc-dice-white.js
+++ module/cnc-dice-white.js
@@ -14,18 +14,18 @@ export class CoyoteDiceWhite extends Die {
     /** @override */
     getResultLabel(result) {
         return {
-            "1": '<img src="systems/coyote-and-crow/ui/dice/w1.png" />',
-            "2": '<img src="systems/coyote-and-crow/ui/dice/w2.png" />',
-            "3": '<img src="systems/coyote-and-crow/ui/dice/w3.png" />',
-            "4": '<img src="systems/coyote-and-crow/ui/dice/w4.png" />',
-            "5": '<img src="systems/coyote-and-crow/ui/dice/w5.png" />',
-            "6": '<img src="systems/coyote-and-crow/ui/dice/w6.png" />',
-            "7": '<img src="systems/coyote-and-crow/ui/dice/w7.png" />',
-            "8": '<img src="systems/coyote-and-crow/ui/dice/w8.png" />',
-            "9": '<img src="systems/coyote-and-crow/ui/dice/w9.png" />',
-            "10": '<img src="systems/coyote-and-crow/ui/dice/w10.png" />',
-            "11": '<img src="systems/coyote-and-crow/ui/dice/w11.png" />',
-            "12": '<img src="systems/coyote-and-crow/ui/dice/w12.png" />',
+            "1": '<img src="systems/coyote-and-crow/ui/dice/chat/w1.png" />',
+            "2": '<img src="systems/coyote-and-crow/ui/dice/chat/w2.png" />',
+            "3": '<img src="systems/coyote-and-crow/ui/dice/chat/w3.png" />',
+            "4": '<img src="systems/coyote-and-crow/ui/dice/chat/w4.png" />',
+            "5": '<img src="systems/coyote-and-crow/ui/dice/chat/w5.png" />',
+            "6": '<img src="systems/coyote-and-crow/ui/dice/chat/w6.png" />',
+            "7": '<img src="systems/coyote-and-crow/ui/dice/chat/w7.png" />',
+            "8": '<img src="systems/coyote-and-crow/ui/dice/chat/w8.png" />',
+            "9": '<img src="systems/coyote-and-crow/ui/dice/chat/w9.png" />',
+            "10": '<img src="systems/coyote-and-crow/ui/dice/chat/w10.png" />',
+            "11": '<img src="systems/coyote-and-crow/ui/dice/chat/w11.png" />',
+            "12": '<img src="systems/coyote-and-crow/ui/dice/chat/w12.png" />',
         }[result.result];
     }
 }
