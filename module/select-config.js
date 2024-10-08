bfefb09494b3f8f457826538c9b36a51264f6e91
Charlotte <38172037+Foxfyre@users.noreply.github.com>
2024-10-08 14:51:33 -0600

    Create select-config.js

---
+++ module/select-config.js
@@ -0,0 +1,104 @@
+export const COYOTE = {};
+
+COYOTE.dropdownChoices = {
+    "": "COYOTE.PathChoices.select",
+    "badger": "COYOTE.PathChoices.badger",
+    "bear": "COYOTE.PathChoices.bear",
+    "beaver": "COYOTE.PathChoices.beaver",
+    "bison": "COYOTE.PathChoices.bison",
+    "coyote": "COYOTE.PathChoices.coyote",
+    "crow": "COYOTE.PathChoices.crow",
+    "deer": "COYOTE.PathChoices.deer",
+    "eagle": "COYOTE.PathChoices.eagle",
+    "falcon": "COYOTE.PathChoices.falcon",
+    "fox": "COYOTE.PathChoices.fox",
+    "owl": "COYOTE.PathChoices.owl",
+    "raccoon": "COYOTE.PathChoices.raccoon",
+    "salmon": "COYOTE.PathChoices.salmon",
+    "snake": "COYOTE.PathChoices.snake",
+    "spider": "COYOTE.PathChoices.spider"
+}
+
+COYOTE.weaponTypes = {
+    "": "COYOTE.WeaponTypes.select",
+    "melee": "COYOTE.WeaponTypes.melee",
+    "ranged": "COYOTE.WeaponTypes.ranged",
+    "unarmed": "COYOTE.WeaponTypes.unarmed"
+}
+
+COYOTE.physicalDefense = {
+    "": "COYOTE.PhysicalDefense.select",
+    "environmental": "COYOTE.PhysicalDefense.environmental"
+}
+
+COYOTE.giftBurden = {
+    "gift": "COYOTE.GiftBurden.gift",
+    "burden": "COYOTE.GiftBurden.burden"
+}
+
+COYOTE.primaryAction = {
+    "primary": "COYOTE.PrimaryAction.primary",
+    "reaction": "COYOTE.PrimaryAction.reaction",
+    "secondary": "COYOTE.PrimaryAction.secondary",
+    "prisec": "COYOTE.PrimaryAction.prisec",
+    "narrative": "COYOTE.PrimaryAction.narrative"
+
+}
+
+COYOTE.stats = {
+    "": "COYOTE.Stats.select",
+    "Agility": "COYOTE.Stats.Agility",
+    "Charisma": "COYOTE.Stats.Charisma",
+    "Endurance": "COYOTE.Stats.Endurance",
+    "Intelligence": "COYOTE.Stats.Intelligence",
+    "Perception": "COYOTE.Stats.Perception",
+    "Spirit": "COYOTE.Stats.Spirit",
+    "Strength": "COYOTE.Stats.Strength",
+    "Will": "COYOTE.Stats.Will",
+    "Wisdom": "COYOTE.Stats.Wisdom",
+    "Wealth": "COYOTE.Stats.Wealth"
+}
+
+COYOTE.dropdownSkills = {
+    "": "COYOTE.Skills.select",
+    "Art": "COYOTE.Skills.Art",
+    "Athletics": "COYOTE.Skills.Athletics",
+    "Ceremony": "COYOTE.Skills.Ceremony",
+    "Charm": "COYOTE.Skills.Charm",
+    "Coercion": "COYOTE.Skills.Coercion",
+    "Computers": "COYOTE.Skills.Computers",
+    "Cooking": "COYOTE.Skills.Cooking",
+    "Crafting": "COYOTE.Skills.Crafting",
+    "Cybernetics": "COYOTE.Skills.Cybernetics",
+    "Deception": "COYOTE.Skills.Deception",
+    "Farming": "COYOTE.Skills.Farming",
+    "Herbalism": "COYOTE.Skills.Herbalism",
+    "Husbandry": "COYOTE.Skills.Husbandry",
+    "Investigation": "COYOTE.Skills.Investigation",
+    "Knowledge": "COYOTE.Skills.Knowledge",
+    "Language": "COYOTE.Skills.Language",
+    "Medicine": "COYOTE.Skills.Medicine",
+    "melee": "COYOTE.Skills.melee",
+    "Music": "COYOTE.Skills.Music",
+    "Performance": "COYOTE.Skills.Performance",
+    "Piloting": "COYOTE.Skills.Piloting",
+    "ranged": "COYOTE.Skills.ranged",
+    "Science": "COYOTE.Skills.Science",
+    "Skulduggery": "COYOTE.Skills.Skulduggery",
+    "Stealth": "COYOTE.Skills.Stealth",
+    "Survival": "COYOTE.Skills.Survival",
+    "Tracking": "COYOTE.Skills.Tracking",
+    "unarmed": "COYOTE.Skills.unarmed"
+}
+
+COYOTE.modifierSN = {
+    "-4": "-4",
+    "-3": "-3",
+    "-2": "-2",
+    "-1": "-1",
+    "0": "0",
+    "1": "+1",
+    "2": "+2",
+    "3": "+3",
+    "4": "+4"
+}
